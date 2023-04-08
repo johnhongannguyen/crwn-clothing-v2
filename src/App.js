21b9203db124cc7e730bd3b5c36fafe18cfc117f
johnhongannguyen <johhaneshongan@gmail.com>
2023-04-08 10:51:31 -0700

    implement sign in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import {Routes, Route} from 'react-router-dom' 
 import Home from './routes/home/home.component.jsx'
+import Navigation  from './routes/navigation/navigation.component.jsx';
+import SignIn from './routes/sign-in/sign-in.component.jsx';
+
+
+const Shop = () =>{
+  return(
+    <h1>
+      I am the shop page
+    </h1>
+  )
+}
+
 
 const App=()=> {
 
   return (
     <Routes>
-      <Route path='/' element={ <Home />} />
-     
+      <Route path='/' element={<Navigation />} >
+      <Route index element={ <Home />} />
+      <Route path='shop' element={<Shop/>} />
+      <Route path='sign-in' element={<SignIn/>} />
+     </Route>
     </Routes>
   );
 }
