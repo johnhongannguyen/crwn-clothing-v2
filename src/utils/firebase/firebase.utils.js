e621daac7487766e5845d5ff17ae1c29ec506728
johnhongannguyen <johhaneshongan@gmail.com>
2023-04-09 16:59:35 -0700

    added sign in component as well as firebase utils and brought in firebase library

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,61 @@
+import {initializeApp} from 'firebase/app';
+import {getAuth,
+     signInWithRedirect,
+     signInWithPopup,
+    GoogleAuthProvider} from 'firebase/auth'
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from'firebase/firestore'
+const firebaseConfig = {
+    apiKey: "AIzaSyAcRqu0o8vfVxg5ajirrhzANj2ei1Ahdto",
+    authDomain: "crwn-clothing-db-4529f.firebaseapp.com",
+    projectId: "crwn-clothing-db-4529f",
+    storageBucket: "crwn-clothing-db-4529f.appspot.com",
+    messagingSenderId: "736670810511",
+    appId: "1:736670810511:web:c59751003017ef1d994bfa"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const provider = new GoogleAuthProvider();
+  provider.setCustomParameters({
+    prompt: 'select_account'
+  });
+
+  export const auth = getAuth();
+  export const signInWithGooglePopup = () => signInWithPopup(auth,provider);
+
+  export const db = getFirestore();
+  export const createUserDocumentFromAuth = async(userAuth) =>{
+    const userDocRef = doc(db,'users',userAuth.uid);
+    console.log(userDocRef);
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    // if user data does not exist
+    //create/ set doc with the data from userAuth in the collection
+    if(!userSnapshot.exists()){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+        try{
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+
+        }catch(error){
+            console.log('error creating the user', error.message);
+        }
+    }
+    
+    // if user data exists
+    // return userDocRef
+    return userDocRef;
+ 
+  }
